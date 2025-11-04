d3fcf25649ab54272ac8310a1f0a896667bc4a7b
AlexisTorruco <altorrucoc@hotmail.com>
2025-11-03 19:16:17 -0600

    Se cambia la config TypeOrm al archivo src/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
+// src/app.module.ts
+//? Configuración principal de la aplicación NestJS y conexión a la base de datos usando TypeORM.
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { ClientModule } from './client/client.module';
 import { ProviderModule } from './provider/provider.module';
@@ -9,6 +13,8 @@ import { SuscriptionModule } from './suscription/suscription.module';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { CategoriesModule } from './categories/categories.module';
 
+import { getTypeOrmOptions } from './config/typeorm.config';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -16,40 +22,16 @@ import { CategoriesModule } from './categories/categories.module';
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
     }),
 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT ?? '6543', 10), //para supabase puerto 6543
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-
-      // Descubre entidades automáticamente
-      autoLoadEntities: true,
-      // entities: ['src/**/*.entity{.ts,.js}'],
-
-      // migrations: ['src/migrations/**/*{.ts,.js}'],
-
-      // Dev: sincroniza; Prod: usa migraciones
-      synchronize: process.env.NODE_ENV === 'development' ? true : false,
-      logging: process.env.NODE_ENV === 'development' ? false : false,
-      dropSchema: process.env.NODE_ENV === 'development' ? false : false,
-      ssl:
-        process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => getTypeOrmOptions(),
     }),
 
     AuthModule,
-
     ClientModule,
-
     ProviderModule,
-
     AdminModule,
-
     SuscriptionModule,
-
     AppointmentsModule,
-
     CategoriesModule,
   ],
 })
