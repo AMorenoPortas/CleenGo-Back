fff2050e9700f4ed9b43407000d7990b5a423f1b
AlexisTorruco <altorrucoc@hotmail.com>
2025-11-27 22:39:48 -0600

    Avance de endpoints users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,37 +6,68 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
+  Request,
+  Put,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enum/role.enum';
+import { AuthenticatedClient } from './interfaces/authenticated-client';
+import { get } from 'http';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
+interface AuthenticatedRequest extends Request {
+  user: AuthenticatedClient;
+}
+
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() registerUserDto: RegisterUserDto) {
-    return this.userService.create(registerUserDto);
-  }
+  //* 1. Como cliente quiero ver mi perfil de usuario (dashboard)
+  @Get('profile/:id')
+  @Roles(Role.CLIENT, Role.ADMIN)
+  getClientProfile(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Request() req: AuthenticatedRequest,
+  ) {}
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
+  //* 2. Como cliente quiero poder modificar mis datos del perfile en el dashboard
+  @Put('update-profile/:id')
+  updateClientProfile(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Request() req: AuthenticatedRequest,
+  ) {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
+  //* 3. Como admin quiero poder ver todos los clientes registrados y filtrarlos por nombre, email o estado (activo/inactivo).
+  @Get('all-clients')
+  getAllClients(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+    @Query('filter') filter: string,
+  ) {}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
+  //* 11. Como cliente quiero poder eliminar mi cuenta de usuario (módulo user).
+  @Delete('delete-profile/:id')
+  @Roles(Role.CLIENT, Role.ADMIN)
+  deleteClientProfile(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Request() req: AuthenticatedRequest,
+  ) {}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
+  //* 13. Como cliente quiero poder cambiar mi contraseña (módulo autenticación).
+
+  //* 14. Como cliente quiero poder recuperar mi contraseña si la olvido (módulo autenticación).
+
+  //* 15. Como cliente quiero poder cerrar sesión de manera segura (módulo de autenticación).
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,24 +3,4 @@ import { RegisterUserDto } from './dto/register-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UserService {
-  create(registerUserDto: RegisterUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
-}
+export class UserService {}
