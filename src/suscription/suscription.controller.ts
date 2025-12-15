4312cdb9657ab25e9ee84de2f6f70d50e97c349e
Sasha Davila <sasha@example.com>
2025-12-15 10:59:34 -0300

    stripe

--- src/suscription/suscription.controller.ts
+++ src/suscription/suscription.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Post, Req, Headers, Body, BadRequestException } from '@nestjs/common';
+import { Controller, Post, Req, Headers, Body, BadRequestException, UseGuards } from '@nestjs/common';
 import { SuscriptionService } from './suscription.service';
 import Stripe from 'stripe';
 import { CreateCheckoutDto } from './dto/create-checkout.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('subscription')
 export class SuscriptionController {
   constructor(private readonly subscriptionService: SuscriptionService) {}
+
+
+  @Post('create-checkout-session')
+async createCheckout(@Body() body: CreateCheckoutDto) {
+  return this.subscriptionService.createCheckoutSession(body.providerId);
+}
 
 /*   @Post('create-checkout-session')
   async createCheckout(@Req() req) {
@@ -13,13 +20,18 @@ export class SuscriptionController {
     return this.subscriptionService.createCheckoutSession(providerId);
   } */
 
-@Post('create-checkout-session')
+/* @Post('create-checkout-session')
   async createCheckout(@Body() body: CreateCheckoutDto) {
     const { providerId } = body;
     if (!providerId) throw new BadRequestException('providerId is required');
     return this.subscriptionService.createCheckoutSession(providerId);
-  }
-
+  } */
+/*   @UseGuards(JwtAuthGuard)
+  @Post('create-checkout-session')
+  async createCheckout(@Req() req: Request) {
+    const providerId = req.user['id']; // ✅ viene del JWT
+    return this.subscriptionService.createCheckoutSession(providerId);
+  } */
   @Post('webhook')
   async stripeWebhook(@Req() req, @Headers('stripe-signature') signature: string) {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2025-11-17.clover' });
